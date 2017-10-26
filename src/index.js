04da6a58acd24444aafc8185581e5551b22bd482
Bailey <sbailey@highland.com>
2017-10-26 18:23:05 -0500

    Added mocking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral';
+// Populate table of users via API call.
+getUsers().then(result => {
+  //let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-//debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+  // result.foreach(user => {
+    // usersBody += `<tr>
+    // <td><a href="#" data-id="${result.user[0].id}" class="deleteUser">Delete</a></td>
+    // <td>${result.user[0].id}</td>
+    // <td>${result.user[0].firstName}</td>
+    // <td>${result.user[0].lastName}</td>
+    // <td>${result.user[0].email}</td>
+    // </tr>`
+  // });
+
+  // result.array.forEach(function(user) {
+  //   usersBody += `<tr>
+  //   <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+  //   <td>${user.id}</td>
+  //   <td>${user.firstName}</td>
+  //   <td>${user.lastName}</td>
+  //   <td>${user.email}</td>
+  //   </tr>`
+  // }, this);
+
+  global.document.getElementById('users').innerHTML = "hi";
+});
 
